10a05767a0a4cf9a427ada19cdfb51971e22952d
vishnu292 <47337198+vishnu292@users.noreply.github.com>
2019-10-25 20:59:42 -0400

    writing Hero code

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    static public Hero S; // Singleton
+
+    [Header("Set in Inspector")]
+
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+
+    public float shieldLevel = 1;
+
+    void Awake() {
+        if (S == null)
+        {
+            S = this; // set the singleton
+        }
+        else {
+            Debug.LogError("Hero.Awake() - Attempted to assignt second Hero.S!");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +35,11 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.detlaTime;
     }
 }
