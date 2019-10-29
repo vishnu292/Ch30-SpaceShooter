eb1c8ae46af65b4de0120610c7bacec1a03a1821
vishnu292 <47337198+vishnu292@users.noreply.github.com>
2019-10-29 18:48:08 -0400

    Hero Script

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -38,8 +38,12 @@ public class Hero : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        Vector3pos = transform.position;
+        Vector3 pos = transform.position;
         pos.x += xAxis * speed * Time.deltaTime;
-        pos.y += yAxis * speed * Time.detlaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
+
 }
